c33bfb10ac7d455963188e7fdd547f35d1d4af94
luis.nunes <luis.nunes@mail.uft.edu.br>
2022-10-08 18:40:19 -0300

    :triangular_flag_on_post: update : start add header mobile

---
+++ src/components/Header/Navigation/index.tsx
@@ -0,0 +1,90 @@
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import {
+  FaBookDead,
+  FaChevronDown,
+  FaMagic,
+  FaSmileBeam,
+  FaUserNinja,
+} from "react-icons/fa";
+import { MdFilter9Plus } from "react-icons/md";
+
+import style from "./style.module.scss";
+import Link from "next/link";
+
+export function Navigation() {
+
+  return (
+    <NavigationMenu.Root className={style.navigation}>
+      <NavigationMenu.List className={style.list}>
+        <NavigationMenu.Item className={style.item}>
+          <Link href="/">
+            <a>Inicio</a>
+          </Link>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item className={style.item}>
+          <Link href="/">
+            <a>Doação</a>
+          </Link>
+        </NavigationMenu.Item>
+
+        <NavigationMenu.Item className={style.item}>
+            <NavigationMenu.Trigger className={`${style.item} ${style.trigger}`} >
+                Gênero
+                <FaChevronDown />
+            </NavigationMenu.Trigger>
+
+          <NavigationMenu.Content>
+            <NavigationMenu.Sub>
+              <NavigationMenu.List className={style.dropdown}>
+                <NavigationMenu.Item className={style.dropdownItem}>
+                  <Link href={"acao"}>
+                    <a>
+                      <FaUserNinja />
+                      Ação
+                    </a>
+                  </Link>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item className={style.dropdownItem}>
+                  <Link href={"/acao"}>
+                    <a>
+                      <FaSmileBeam />
+                      Comédia
+                    </a>
+                  </Link>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item className={style.dropdownItem}>
+                  <Link href={"/acao"}>
+                    <a>
+                      <FaBookDead />
+                      Shounen
+                    </a>
+                  </Link>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item className={style.dropdownItem}>
+                  <Link href={"/acao"}>
+                    <a>
+                      <FaMagic />
+                      Magia
+                    </a>
+                  </Link>
+                </NavigationMenu.Item>
+                <NavigationMenu.Item className={style.dropdownItem}>
+                  <Link href={"/generos"}>
+                    <a>
+                      <MdFilter9Plus />
+                      Outros
+                    </a>
+                  </Link>
+                </NavigationMenu.Item>
+              </NavigationMenu.List>
+              <NavigationMenu.Viewport />
+            </NavigationMenu.Sub>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
+
+      <NavigationMenu.Viewport />
+    </NavigationMenu.Root>
+  );
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,16 @@
 import Image from "next/future/image";
 import Link from "next/link";
 import style from "./style.module.scss"
-import { FaChevronDown } from 'react-icons/fa'
-import { DropdownGenres } from "./DropdownGenres";
+import { Navigation } from "./Navigation";
+import useWindowDimesions from "../../hooks/useWindowDimensions";
+// import { useWindowDimesions } from "../../hooks/useWindowDimensions";
 
 export function Header() {
+    const [ width ] = useWindowDimesions()
+
+    // console.log(width)
+    const isMobile = width < 800
+
     return (
         <header className={style.headerContainer}>
             <div className={style.header}>
@@ -12,23 +18,15 @@ export function Header() {
                     Meu kyuden
                 </h1>
 
-                <nav className={style.menu}>
-                    <ul className={style.menuContainer}>
-                        <li className={style.menu__item}>
-                            <Link href={"/"}>Inicio</Link>
-                        </li>
-                        <li className={style.menu__item}>
-                            <Link href={"/minha-lista"}>Doação</Link>
-                        </li>
-                        <li className={style.menu__item}>
-                            <DropdownGenres />                             
-                        </li>
-                    </ul>
-                </nav>
+                { isMobile ? (
+                    <h1>.</h1>
+                ) : (
+                    <Navigation />
+                )}
 
-                <div>
+                <div className={style.userAndSearch}>
                     <div className={style.user}>
-                        <span>Bem vindo, <strong>Luis Felipe</strong></span>
+                        { !isMobile && <span>Bem vindo, <strong>Luis Felipe</strong></span>}
                         <Image 
                             src="/avatar.png"
                             width={50}
