1d4007c6a794a2fcce62d46f2782cfacac31a4c2
Luis-Felipe-N <luisj2felipe09@gmail.com>
2023-05-04 22:24:12 -0300

    :lipstick: add icons in menu mobile

--- src/components/Header/Navigation/index.tsx
+++ src/components/Header/Navigation/index.tsx
@@ -2,16 +2,21 @@ import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import {
   FaBookDead,
   FaChevronDown,
+  FaGlobeAmericas,
   FaMagic,
+  FaRegMoneyBillAlt,
+  FaSearch,
   FaSmileBeam,
   FaUserNinja,
 } from "react-icons/fa";
+
 import { MdFilter9Plus } from "react-icons/md";
+import { BsHouseFill } from "react-icons/bs";
 
 import style from "./style.module.scss";
 import Link from "next/link";
 import useWindowDimesions from "../../../hooks/useWindowDimensions";
-import { isMobile } from "../../../utils/Device";
+import { ActiveLink } from "../../ActiveLink";
 
 interface INavigationProps {
   setStateMenu: (value: boolean) => void,
@@ -34,28 +39,47 @@ export function Navigation({ setStateMenu, stateMenu }: INavigationProps) {
     <NavigationMenu.Root className={style.navigation}>
       <NavigationMenu.List className={style.list}>
         <NavigationMenu.Item className={style.item}>
-          <Link href="/" >
-            <a className={style.active} onClick={handleCloseMenu}>Inicio</a>
-          </Link>
+          <ActiveLink activeClassName={style.active} href="/" >
+            <a className={style.active} onClick={handleCloseMenu}>
+              <span className={style.item__icon}>
+                <BsHouseFill size={20} />
+              </span>
+              Inicio
+            </a>
+          </ActiveLink>
         </NavigationMenu.Item>
 
         <NavigationMenu.Item className={style.item}>
-          <Link href="/">
-            <a onClick={handleCloseMenu}>Doação</a>
-          </Link>
+          <ActiveLink activeClassName={style.active} href="/doacao">
+            <a onClick={handleCloseMenu}>
+              <span className={style.item__icon}>
+                <FaRegMoneyBillAlt size={20} />
+              </span>
+              Doação
+            </a>
+          </ActiveLink>
         </NavigationMenu.Item>
 
         <NavigationMenu.Item className={style.item}>
-          <Link href="/pesquisa">
-            <a onClick={handleCloseMenu}>Pesquisa</a>
-          </Link>
+          <ActiveLink activeClassName={style.active} href="/pesquisa">
+            <a onClick={handleCloseMenu}>
+              <span className={style.item__icon}><FaSearch size={20}/></span>
+              Pesquisa
+            </a>
+          </ActiveLink>
         </NavigationMenu.Item>
 
         { width < 700 ? (
           <NavigationMenu.Item className={style.item}>
-            <Link href="/generos">
-              <a onClick={handleCloseMenu}>Gênero</a>
-            </Link>
+            <ActiveLink activeClassName={style.active} href="/generos">
+              
+              <a onClick={handleCloseMenu}>
+                <span className={style.item__icon}>
+                <FaGlobeAmericas size={20} />
+                </span>
+                Gênero
+              </a>
+            </ActiveLink>
           </NavigationMenu.Item>
         ) : (
           <>
